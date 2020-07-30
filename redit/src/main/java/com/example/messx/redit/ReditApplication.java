381d6520274d3313fa0324e077f2d7e5fc75efc9
Muskaan <muskaan.bansal@kaplan.com>
2020-07-30 23:14:00 +0530

    new todo app code

---
+++ redit/src/main/java/com/example/messx/redit/ReditApplication.java
@@ -0,0 +1,13 @@
+package com.example.messx.redit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReditApplication.class, args);
+	}
+
+}
